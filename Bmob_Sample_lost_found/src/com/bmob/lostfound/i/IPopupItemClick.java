04837f0b5c81c7605a74e153c6b4023ea316ce00
peter <2509825661@qq.com>
2017-04-19 22:24:33 +0800

    Initial commit

---
+++ Bmob_Sample_lost_found/src/com/bmob/lostfound/i/IPopupItemClick.java
@@ -0,0 +1,15 @@
+package com.bmob.lostfound.i;
+
+import android.view.View;
+
+/** �����༭�ӿ�
+  * @ClassName: IPopupItemClick
+  * @Description: TODO
+  * @author smile
+  * @date 2014-5-21 ����
+  */
+public interface IPopupItemClick {
+	void onEdit(View v);
+
+	void onDelete(View v);
+}
